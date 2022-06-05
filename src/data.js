5353cc8c2392dfa6036df1755cac2474ab7f395f
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-06-05 18:07:07 -0400

    add projects to data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "Herbiview",
+    subtitle: "React Plant Learning",
+    description:
+      "An application that interacts with the Plant ID API to retrieve plant information based on images uploaded by the user.",
+    image: "./logo192.png",
+    link: "https://github.com/nathan-ts/herbiview",
+  },
+  {
+    title: "Schoodle",
+    subtitle: "JS Event Scheduler",
+    description:
+      "An event scheduling app for organizers and attendees alike to share their availability.",
+      image: "./logo192.png",
+    link: "https://github.com/nathan-ts/schoodle",
+  },
+  {
+    title: "Scheduler",
+    subtitle: "React Interview Scheduler",
+    description:
+      "An interview scheduling app.",
+      image: "./logo192.png",
+    link: "https://github.com/nathan-ts/scheduler",
+  },
+  {
+    title: "TinyApp",
+    subtitle: "JS Url Shortener",
+    description:
+      "A URL shortener app, with login and rudimentary statistics.",
+      image: "./logo192.png",
+    link: "https://github.com/nathan-ts/tinyapp",
+  },
+];
