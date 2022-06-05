8d6edec3c9d6f06aa3d8983adebab15fac4486e8
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-06-05 18:01:49 -0400

    add about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,42 @@
+// src/components/About.js
+
+import React from "react";
 
 export default function About() {
-
-};
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Nathan.
+            <br className="hidden lg:inline-block" />
+            I'm always looking to optimize.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            I think the only card she has is the Lorem card. My text is long and beautiful, as, it has been well documented, are various other parts of my website. 
+            Despite the constant negative ipsum covfefe. You know, it really doesn’t matter what you write as long as you’ve got a young, and beautiful, piece of text.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Reach out!
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              What I'm up to
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./portrait.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
