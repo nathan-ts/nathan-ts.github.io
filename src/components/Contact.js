2665a5ede11fe4ef16078444d30e1c8184c8acae
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-06-10 17:36:50 -0400

    revise spacing on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,34 +37,14 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="text-center mb-10">
+      <div className="text-center mb-12">
         <InboxInIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
           Contact
         </h1>
       </div>
-      <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-        <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-          <FontAwesomeIcon icon={faEnvelope} /> EMAIL
-        </h2>
-        <a className="text-indigo-400 leading-relaxed" href="mailto:hi@nathantsang.dev">
-          hi@nathantsang.dev
-        </a>
-        <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-          <FontAwesomeIcon icon={faLinkedin} /> LINKEDIN
-        </h2>
-        <a className="text-indigo-400 leading-relaxed" href="https://www.linkedin.com/in/npytsang/" target="_blank" rel="noreferrer">
-          linkedin.com/in/npytsang
-        </a>
-        <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-          <FontAwesomeIcon icon={faGithub} /> GITHUB
-        </h2>
-        <a className="text-indigo-400 leading-relaxed" href="https://github.com/nathan-ts/" target="_blank" rel="noreferrer">
-          github.com/nathan-ts
-        </a>
-      </div>
       <div className="container px-6 py-6 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 mx-auto flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
@@ -92,6 +72,26 @@ export default function Contact() {
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <div className="lg:w-1/2 mb-6 lg:mt-0">
+            <h2 className="title-font font-semibold text-white tracking-widest text-s">
+              <FontAwesomeIcon icon={faEnvelope} /> EMAIL
+            </h2>
+            <a className="text-indigo-400 leading-relaxed" href="mailto:hi@nathantsang.dev">
+              hi@nathantsang.dev
+            </a>
+            <h2 className="title-font font-semibold text-white tracking-widest text-s mt-4">
+              <FontAwesomeIcon icon={faLinkedin} /> LINKEDIN
+            </h2>
+            <a className="text-indigo-400 leading-relaxed" href="https://www.linkedin.com/in/npytsang/" target="_blank" rel="noreferrer">
+              linkedin.com/in/npytsang
+            </a>
+            <h2 className="title-font font-semibold text-white tracking-widest text-s mt-4">
+              <FontAwesomeIcon icon={faGithub} /> GITHUB
+            </h2>
+            <a className="text-indigo-400 leading-relaxed" href="https://github.com/nathan-ts/" target="_blank" rel="noreferrer">
+              github.com/nathan-ts
+            </a>
+          </div>
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me!
           </h2>
